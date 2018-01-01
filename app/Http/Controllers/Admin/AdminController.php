08856036fe176b0758ec33fb6e8754c8310a4da9
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-01-01 22:30:37 +0700

    Menghapus auth check ganjar di AdminController

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -10,114 +10,58 @@ class AdminController extends Controller
 {
     function layout($path, $title)
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view($path, ['title' => $title]);
-        } else {
-            return view('admin/login');
-        }
+        return view($path, ['title' => $title]);
     }
     function index()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/dashboard', ['title' => 'Dashboard']);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/dashboard', ['title' => 'Dashboard']);
     }
     function dashboard()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/dashboard', ['title' => 'Dashboard']);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/dashboard', ['title' => 'Dashboard']);
     }
     function post()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/post', ['title' => 'Add Product']);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/post', ['title' => 'Add Product']);
     }
     function orders()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/orders', ['title' => 'List Orders']);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/orders', ['title' => 'List Orders']);
     }
     function customers()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/customers', ['title' => 'List Customers']);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/customers', ['title' => 'List Customers']);
     }
     function products()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            $newest_products =
-            Product::orderBy('created_at', 'desc')
-                ->take(5)
-                ->get();
-            return view('admin/products', ['title' => 'List Products','newest_products' => $newest_products]);
-        } else {
-            return view('admin/login');
-        }
+        $newest_products =
+        Product::orderBy('created_at', 'desc')
+            ->take(5)
+            ->get();
+
+        return view('admin/products', [
+            'title' => 'List Products',
+            'newest_products' => $newest_products,
+        ]);
     }
     function categories()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/categories', ['title' => 'Categories']);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/categories', ['title' => 'Categories']);
     }
     function setting()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/setting', ['title' => 'Setting']);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/setting', ['title' => 'Setting']);
     }
     function info()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/info', ['title' => 'Info']);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/info', ['title' => 'Info']);
     }
     function profile()
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/profile', ['title' => 'Profile']);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/profile', ['title' => 'Profile']);
     }
     function customer($idcustomer)
     {
-        $idadmin = session()->get('idadmin');
-        if (!empty($idadmin)) {
-            return view('admin/customerProfile', ['title' => 'Customer Profile ',$idcustomer]);
-        } else {
-            return view('admin/login');
-        }
+        return view('admin/customerProfile', ['title' => 'Customer Profile ',$idcustomer]);
     }
 }
