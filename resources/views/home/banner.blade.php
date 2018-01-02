e77b958113264b653b04238632434640303364c1
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2018-01-02 09:19:06 +0700

    penambahan banner

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -6,5 +6,7 @@
     <div class="right">
         <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner2.jpg') }}');"></div>
         <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner3.jpg') }}');"></div>
+        <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner4.jpg') }}');"></div>
+        <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner5.jpg') }}');"></div>
     </div>
 </div>
