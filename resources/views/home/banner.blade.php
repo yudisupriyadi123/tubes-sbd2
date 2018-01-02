5415e3224c9cf96298a80897b7391d7717250c4d
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2018-01-02 09:09:38 +0700

    perubahan benner

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,22 +1,10 @@
 <div class="home-articles">
     <div class="left">
-        <a href="{{ url('product/12045') }}">
-            <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner1.jpg') }}');"></div>
-        </a>
+        <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner1.jpg') }}');"></div>
     </div>
 
     <div class="right">
-        <a href="{{ url('product/12045') }}">
-            <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner1.jpg') }}');"></div>
-        </a>
-        <a href="{{ url('product/12045') }}">
-            <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner2.jpg') }}');"></div>
-        </a>
-        <a href="{{ url('product/12045') }}">
-            <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner1.jpg') }}');"></div>
-        </a>
-        <a href="{{ url('product/12045') }}">
-            <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner2.jpg') }}');"></div>
-        </a>
+        <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner2.jpg') }}');"></div>
+        <div class="frame-articles need-zoom" style="background-image: url('{{ asset('/img/banner3.jpg') }}');"></div>
     </div>
 </div>
