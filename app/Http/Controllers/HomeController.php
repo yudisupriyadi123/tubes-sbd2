4ad40db33bca475a5c1b1dbf2ebec67d7b2a75a9
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-01-02 08:39:32 +0700

    Pakai Product::getAll()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
         $max_product = 5;
         $newest_products = Product::getNewests($max_product);
         $biggest_discount_products = Product::getBiggestDiscounted($max_product);
-        $all_product = GetpostModel::AllProduct($max_product);
+        $all_product = Product::getAll($max_product);
         $category = CategoryModel::GetVal(7);
 
         return view('/home/index', [
