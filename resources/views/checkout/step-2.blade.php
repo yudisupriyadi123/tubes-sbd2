cb651c79aef4e3426f5fb5202df08d417895ebc0
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2018-01-08 20:47:16 +0700

    Ubah pesan di checkout step-2

--- resources/views/checkout/step-2.blade.php
+++ resources/views/checkout/step-2.blade.php
@@ -1,8 +1,11 @@
 @extends('layout.index')
 @section('title', $title)
 @section('content')
+    <!-- TODO: change id with correct semantic -->
     <div id="error-405">
         <p>Your Order ID:</p>
         {{ md5($trans_id) }}
+
+        Please send your money to <b>BNI (100-1234-888)</b> then upload your proof photo. Admin will review that and then will send your product.
     </div>
 @endsection
